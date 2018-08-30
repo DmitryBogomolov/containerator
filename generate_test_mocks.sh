86235f8c9cfcb85d724bfb5f8bf3065dd4497783
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2018-08-30 22:43:53 +0300

    Add mocks under source control

--- generate_test_mocks.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-mockgen -destination test_mocks/imageapiclient.go -package test_mocks github.com/docker/docker/client ImageAPIClient
-mockgen -destination test_mocks/containerapiclient.go -package test_mocks github.com/docker/docker/client ContainerAPIClient
