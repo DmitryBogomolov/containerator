28327c3afe440c1fe82eab95640fe5c92e081c04
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2018-12-04 15:11:34 +0300

    Change status code

--- examples/manage_container_server/main.go
+++ examples/manage_container_server/main.go
@@ -29,7 +29,7 @@ const apiTagsRoute = "/api/tags/"
 func restrictMethod(method string, handler http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != method {
-			http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+			http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
 			return
 		}
 		handler.ServeHTTP(w, r)
