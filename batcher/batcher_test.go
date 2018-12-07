4d70193f19204cacaaed1740a6f7433279905a60
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2018-12-07 14:23:56 +0300

    Add tests

--- batcher/batcher_test.go
+++ batcher/batcher_test.go
@@ -1,1 +1,33 @@
 package batcher
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSingleInvocation(t *testing.T) {
+	called := false
+	b := NewBatcher(func() {
+		called = true
+	})
+
+	b.Invoke()
+
+	assert.Equal(t, true, called)
+}
+
+func TestTwoInvocations(t *testing.T) {
+	count := 0
+	move := make(chan int)
+	b := NewBatcher(func() {
+		<-move
+		count++
+	})
+
+	go b.Invoke()
+	go b.Invoke()
+	move <- 0
+	assert.Equal(t, 1, count)
+
+}
