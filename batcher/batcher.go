01666bd9f77fb1e81917d2873af77ee2c97a9480
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2018-12-07 14:11:32 +0300

    Add type

---
+++ batcher/batcher.go
@@ -0,0 +1,54 @@
+// Package batcher contains batcher type that replaces several similar long running
+// invocations with a single one.
+package batcher
+
+import (
+	"sync"
+)
+
+// Batcher replaces several long running function invocations with a single one.
+type Batcher struct {
+	wg     sync.WaitGroup
+	mux    sync.Mutex
+	locker int
+	action func()
+}
+
+// NewBatcher creates an instance of Batcher.
+func NewBatcher(action func()) *Batcher {
+	return &Batcher{action: action}
+}
+
+func (b *Batcher) lock() bool {
+	b.mux.Lock()
+	defer b.mux.Unlock()
+	if b.locker < 0 {
+		panic("negative locker")
+	}
+	if b.locker > 0 {
+		b.locker++
+		return false
+	}
+	b.locker = 1
+	b.wg.Add(1)
+	return true
+}
+
+func (b *Batcher) unlock() {
+	b.mux.Lock()
+	defer b.mux.Unlock()
+	b.locker--
+	if b.locker < 0 {
+		panic("negative locker")
+	}
+}
+
+// Invoke calls batcher action.
+func (b *Batcher) Invoke() {
+	if b.lock() {
+		b.action()
+		b.wg.Done()
+	}
+	b.wg.Wait()
+	b.unlock()
+}
