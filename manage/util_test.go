8a859b2d4e8b56df842a6e59a3f3ee47a55f00de
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2023-04-19 11:17:37 +0300

    Remove redundant code

--- manage/util_test.go
+++ manage/util_test.go
@@ -64,13 +64,13 @@ func TestFindImage(t *testing.T) {
 	defer ctrl.Finish()
 
 	testImages := []types.ImageSummary{
-		types.ImageSummary{
+		{
 			RepoTags: []string{"test-image:1"},
 		},
-		types.ImageSummary{
+		{
 			RepoTags: []string{"test-image:2"},
 		},
-		types.ImageSummary{
+		{
 			RepoTags: []string{"test-image:3"},
 		},
 	}
@@ -130,8 +130,8 @@ func TestBuildContainerOptions(t *testing.T) {
 			RestartPolicy: containerator.RestartAlways,
 			Network:       "test-net",
 			Ports: []containerator.Mapping{
-				containerator.Mapping{Source: "210", Target: "1"},
-				containerator.Mapping{Source: "211", Target: "2"},
+				{Source: "210", Target: "1"},
+				{Source: "211", Target: "2"},
 			},
 		}
 		actual := buildContainerOptions(&Config{
