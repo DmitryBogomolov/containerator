1864536dbe9ec59a6b525acef60412ac7a001516
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2023-04-20 10:58:49 +0300

    Move 'generate' comments

--- core/cli_wrapper.go
+++ core/cli_wrapper.go
@@ -9,6 +9,9 @@ import (
 	"github.com/docker/docker/client"
 )
 
+//go:generate mockgen -destination ../test_mocks/mock_imageapiclient.go -package test_mocks github.com/docker/docker/client ImageAPIClient
+//go:generate mockgen -destination ../test_mocks/mock_containerapiclient.go -package test_mocks github.com/docker/docker/client ContainerAPIClient
+
 const (
 	contextTimeout = 10 * time.Second
 )
--- core/gen_mocks_test.go
+++
@@ -1,4 +0,0 @@
-package core_test
-
-//go:generate mockgen -destination ./test_mocks/mock_imageapiclient.go -package test_mocks github.com/docker/docker/client ImageAPIClient
-//go:generate mockgen -destination ./test_mocks/mock_containerapiclient.go -package test_mocks github.com/docker/docker/client ContainerAPIClient
