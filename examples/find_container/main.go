1f3bedb5210ba8b90ac485a28602ed763dd4345b
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2023-04-24 21:11:11 +0300

    Update 'find_container' demo

--- examples/find_container/main.go
+++ examples/find_container/main.go
@@ -10,13 +10,67 @@ import (
 	"github.com/docker/docker/client"
 )
 
+func displayContainer(container core.Container) string {
+	return fmt.Sprintf("%s (%s)", container.Name(), container.ShortID())
+}
+
+func findContainerByID(cli *client.Client, id string) error {
+	container, err := core.FindContainerByShortID(cli, id)
+	if _, ok := err.(*core.ContainerNotFoundError); ok {
+		fmt.Println("Container not found")
+		return nil
+	}
+	if err != nil {
+		return err
+	}
+	fmt.Println(displayContainer(container))
+	return nil
+}
+
+func findContainerByName(cli *client.Client, name string) error {
+	container, err := core.FindContainerByName(cli, name)
+	if _, ok := err.(*core.ContainerNotFoundError); ok {
+		fmt.Println("Container not found")
+		return nil
+	}
+	if err != nil {
+		return err
+	}
+	fmt.Println(displayContainer(container))
+	return nil
+}
+
+func findContainersByImageID(cli *client.Client, imageID string) error {
+	containers, err := core.FindContainersByImageID(cli, imageID)
+	if err != nil {
+		return err
+	}
+	for _, container := range containers {
+		fmt.Println(displayContainer(container))
+	}
+	return nil
+}
+
+func listAllContainers(cli *client.Client) error {
+	containerIDs, err := core.ListAllContainerIDs(cli)
+	if err != nil {
+		return err
+	}
+	for _, id := range containerIDs {
+		fmt.Println(id)
+	}
+	return nil
+}
+
 func run() error {
 	var id string
-	flag.StringVar(&id, "id", "", "id")
+	flag.StringVar(&id, "id", "", "container by d")
 	var name string
-	flag.StringVar(&name, "name", "", "name")
+	flag.StringVar(&name, "name", "", "container by name")
 	var imageID string
-	flag.StringVar(&imageID, "image-id", "", "image id")
+	flag.StringVar(&imageID, "image-id", "", "containers by image id")
+	var listAll bool
+	flag.BoolVar(&listAll, "list-all", false, "list all containers")
 
 	flag.Parse()
 
@@ -26,43 +80,22 @@ func run() error {
 	}
 
 	if id != "" {
-		container, err := core.FindContainerByID(cli, id)
-		if err != nil {
-			return err
-		}
-		if container == nil {
-			fmt.Println("Container is not found.")
-		} else {
-			fmt.Printf("Container: %s\n", container.Name())
-		}
+		return findContainerByID(cli, id)
 	} else if name != "" {
-		container, err := core.FindContainerByName(cli, name)
-		if err != nil {
-			return err
-		}
-		if container == nil {
-			fmt.Println("Container is not found.")
-		} else {
-			fmt.Printf("Container: %s\n", container.Name())
-		}
+		return findContainerByName(cli, name)
 	} else if imageID != "" {
-		containers, err := core.FindContainersByImageID(cli, imageID)
-		if err != nil {
-			return err
-		}
-		fmt.Println("Containers:")
-		for _, container := range containers {
-			fmt.Printf("  %s\n", container.Name())
-		}
+		return findContainersByImageID(cli, imageID)
+	} else if listAll {
+		return listAllContainers(cli)
 	} else {
 		flag.Usage()
+		return nil
 	}
-	return nil
 }
 
 func main() {
 	if err := run(); err != nil {
-		fmt.Printf("%+v\n", err)
+		fmt.Println(err)
 		os.Exit(1)
 	}
 }
